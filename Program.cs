bb8d69409e16c02bfcda54824649e83fa9b9b9bd
superlol44445 <leogikonyo@gmail.com>
2025-05-06 02:46:38 +0200

    made http work
    the bat file starts local tunnel

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,74 +15,35 @@ namespace API_Backend
         public int id { get; set; }
         public int cost { get; set; }
         public int amount_bought { get; set; }
-        public int[]? coupon_ids { get; set; }    
+        public int[]? coupon_ids { get; set; }
         public string[]? categories { get; set; }
+    }
+    public static class productData
+    {
+        public static Product[] products;
     }
     internal class Program
     {
-        public static Product[] products;
         static void Main(string[] args)
         {
             string Products_File_Path = "Products.json";
             byte[] Product_Data = File.ReadAllBytes(Products_File_Path);
-            
-            products = JsonSerializer.Deserialize<Product[]>(Product_Data);
+            productData.products = JsonSerializer.Deserialize<Product[]>(Product_Data);
 
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.WebHost.UseKestrel(options => 
+            {
+                options.ListenAnyIP(5000);
+                options.ListenAnyIP(443, listenOptions => listenOptions.UseHttps());
+            });
             builder.Services.AddControllers();
 
             var app = builder.Build();
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseRouting();
             app.MapControllers();
-
-            Thread thread = new Thread(app.Run); 
-            thread.Start();
-            while (true)
-            {
-                Console.WriteLine("11");
-                Thread.Sleep(100);
-            }
-            
-        }
-        [RequireHttps]
-        [ApiController]
-        [Route("api/pagedata")]
-        public class Fetch_Pagedata : ControllerBase
-        {
-            [HttpGet()]
-            [Route("Index")]
-            
-            public IActionResult Index_Data()
-            {
-                IActionResult result;
-                
-
-
-                return result;
-            }
-            [HttpGet()]
-            [Route("products_page")]
-            public IActionResult Products_Page_Data()
-            {
-                IActionResult result;
-
-
-
-                return result;
-            }
-            [HttpGet()]
-            [Route("product")]
-            public IActionResult Product_Data([FromQuery] int id)
-            {
-                IActionResult result;
-
-
-
-                return result;
-            }
+            app.Run();
         }
     }
 }
--- obj/Release/net8.0/Backend.AssemblyInfo.cs
+++ obj/Release/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603502c6f517d4db205b3ce1474aabb70f9b019a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28accaec45b93a63792e99bfd60b9c66fee92cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
